db5949b0850e3a09ad8895c1ed32f683e75427a8
gabisegger <gabi.segger@gmail.com>
2022-05-26 07:51:35 -0300

    feat: create event click to button

--- script.js
+++ script.js
@@ -1,36 +1,45 @@
+const btn = document.getElementById('btn')
+const form = document.getElementById('form')
 
 
+btn.addEventListener('click', (e) => {
+    e.preventDefault()
 
-$.ajax({url: "https://api.nasa.gov/planetary/apod?api_key=PBQuKxC4InPeHSsb8AG6hryDhfnPz1awfLGflQdh",
+    form.style.display = 'none'
 
-success: function(result){
-//renderUser(result);
-    const title = result.title
-    const url = result.hdurl
-    const description = result.explanation
-    const date = result.date
+    $.ajax({url: "https://api.nasa.gov/planetary/apod?api_key=PBQuKxC4InPeHSsb8AG6hryDhfnPz1awfLGflQdh",
 
-    const dateText = $("<span></span>").text(date)
-    $(dateText).insertAfter("h1");
+    success: function(result){
+    //renderUser(result);
+        const title = result.title
+        const url = result.hdurl
+        const description = result.explanation
+        const date = result.date
 
-    const titleText = $("<h2></h2>").text(title)
-    $(titleText).insertAfter("span");
+        const dateText = $("<span></span>").text(date)
+        $(dateText).insertAfter("h1");
 
-    if(result.media_type == "image"){
-        const imgHtml = "<img src='" + url +"' alt=''>"
-        $(imgHtml).insertAfter("h2");
+        const titleText = $("<h2></h2>").text(title)
+        $(titleText).insertAfter("span");
 
-        const descriptionText = $("<p></p>").text(description)
-        $(descriptionText).insertAfter("img");
-    }
+        if(result.media_type == "image"){
+            const imgHtml = "<img src='" + url +"' alt=''>"
+            $(imgHtml).insertAfter("h2");
 
-    if(result.media_type == "video"){
-        const videoHtml = "<video src='"+url+"' autoplay></video>"
-        $(videoHtml).insertAfter("h2");
+            const descriptionText = $("<p></p>").text(description)
+            $(descriptionText).insertAfter("img");
+        }
 
-        const descriptionText = $("<p></p>").text(description)
-        $(descriptionText).insertAfter("video");
-    }
+        if(result.media_type == "video"){
+            const videoHtml = "<video src='"+url+"' autoplay></video>"
+            $(videoHtml).insertAfter("h2");
 
-}});
+            const descriptionText = $("<p></p>").text(description)
+            $(descriptionText).insertAfter("video");
+        }
+
+    }});
+
+
+})
 
