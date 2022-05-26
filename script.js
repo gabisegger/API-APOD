732d5d3cbbfc5823cbb21e3eb77fe449a61c1185
gabisegger <gabi.segger@gmail.com>
2022-05-25 22:27:01 -0300

    feat: insert API contents into HTML

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+
+
 $.ajax({url: "https://api.nasa.gov/planetary/apod?api_key=PBQuKxC4InPeHSsb8AG6hryDhfnPz1awfLGflQdh",
 
 success: function(result){
@@ -6,6 +9,28 @@ success: function(result){
     const url = result.hdurl
     const description = result.explanation
     const date = result.date
+
+    const dateText = $("<span></span>").text(date)
+    $(dateText).insertAfter("h1");
+
+    const titleText = $("<h2></h2>").text(title)
+    $(titleText).insertAfter("span");
+
+    if(result.media_type == "image"){
+        const imgHtml = "<img src='" + url +"' alt=''>"
+        $(imgHtml).insertAfter("h2");
+
+        const descriptionText = $("<p></p>").text(description)
+        $(descriptionText).insertAfter("img");
+    }
+
+    if(result.media_type == "video"){
+        const videoHtml = "<video src='"+url+"' autoplay></video>"
+        $(videoHtml).insertAfter("h2");
+
+        const descriptionText = $("<p></p>").text(description)
+        $(descriptionText).insertAfter("video");
+    }
 
 }});
 
